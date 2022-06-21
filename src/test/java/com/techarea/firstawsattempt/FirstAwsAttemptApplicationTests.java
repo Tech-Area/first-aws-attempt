40b00594fda1bc0b11eb37623cd2dd554a129bcd
steffanciprian <steffanciprian@gmail.com>
2022-06-21 21:16:22 +0300

    Added GET

---
+++ src/test/java/com/techarea/firstawsattempt/FirstAwsAttemptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techarea.firstawsattempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAwsAttemptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
